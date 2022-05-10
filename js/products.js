14d82e5d803bd3ffbaf393f61c239a8ff19464af
David Ramon Thomen <thomendavid1@gmail.com>
2022-05-10 00:10:56 -0300

    Correcion de products.js

--- js/products.js
+++ js/products.js
@@ -40,17 +40,15 @@ const products = [
         stock: 50
     }
 ];
-
+const carrito = [];
 
 products.forEach(product => {
     // Enlazando el div contenedor
     const cards = document.getElementById("products");
-    // Creacion de las etiquetas con sus clases y el contenido
-    const col = document.createElement("div");
-    col.className = "col-md-4 pb-4";
-
+    // Creacion de las etiquetas con sus clases(.className) y el contenido (.innerText)
     const card = document.createElement("div");
-    card.className = "card ";
+    card.className = "card bg-transparent";
+    card.id = `${product.id}`
 
     const img = document.createElement("img");
     img.className = "card-img-top";
@@ -59,63 +57,110 @@ products.forEach(product => {
     const cardBody = document.createElement("div");
     cardBody.className = "card-body";
 
-    const titulo = document.createElement("h5");
-    titulo.className = "card-title";
-    titulo.innerText = product.nombre;
+    const h5 = document.createElement("h5");
+    h5.className = "card-title text-light";
+    h5.innerText = product.nombre;
 
-    const descripcion = document.createElement("p");
-    descripcion.className = "card-text";
-    descripcion.innerText = product.descripcion;
+    const p = document.createElement("p");
+    p.className = "card-text text-light";
+    p.innerText = product.descripcion;
 
-    const small = document.createElement("small");
-    small.className = "text-muted";
-    small.innerHTML = "Stock: " + product.stock + " Unidades <br></br>";
+    const p2 = document.createElement("p");
+    p2.className = "card-text text-light";
+    p2.innerText = product.precio;
 
-    const container = document.createElement("div");
-    container.className = "container-fluid";
+    const divRight = document.createElement("div");
+    divRight.className = "text-right"
 
-    const row = document.createElement("div");
-    row.className = "row justify-content-between";
+    const button = document.createElement("button");
+    button.className = "btn btn-info";
+    button.innerText = "Comprar";
 
-    const colPrecio = document.createElement("div");
-    colPrecio.className = "col alignt-middle";
+    const divRight2 = document.createElement("div");
+    divRight2.className = "text-right"
 
-    const precio = document.createElement("p");
-    precio.className = "card-text";
-    precio.innerText = "$ " + product.precio;
+    // const div = document.createElement("div");
+    // const div2 = document.createElement("div");
 
-    const colButton = document.createElement("div");
-    colButton.className = "col text-right";
+    const button3 = document.createElement("button");
+    button3.className = "badge-pill badge-light py-1";
+    button3.innerText = "Ver Carrito";
 
-    const comprarButton = document.createElement("button");
-    comprarButton.className = "btn btn-info";
-    comprarButton.innerText = "Comprar";
+    const button2 = document.createElement("button");
+    button2.className = "badge-pill badge-light py-1";
+    button2.innerText = "Agregar al Carrito";
+
+    const icon = document.createElement("i");
+    icon.className = "bi bi-cart-plus ri-xl"
 
     const cardFooter = document.createElement("div");
     cardFooter.className = "card-footer";
 
-    const textRight = document.createElement("div");
-    textRight.className = "text-right";
-    
-    const addCartBtn = document.createElement("a");
-    addCartBtn.href = "#"
-    addCartBtn.className = "text-decoration-none badge-pill badge-light py-1";
-    addCartBtn.innerHTML = "Añadir al carro <i class=\"bi bi-cart-plus ri-xl\"></i>";
+    // const small = document.createElement("small");
+    // small.className = "text-muted";
+    // small.innerText = product.stock
 
-    cards.append(col);
-    col.append(card);
+    cards.append(card);
     card.append(img, cardBody, cardFooter);
-    cardBody.append(titulo, descripcion, small, container);
-    container.append(row);
-    row.append(colPrecio, colButton);
-    colPrecio.append(precio);
-    colButton.append(comprarButton);
-    cardFooter.append(textRight);
-    textRight.append(addCartBtn);
-
+    cardBody.append(h5, p, p2, divRight);
+    divRight.append(button);
+    cardFooter.append(divRight2);
+    divRight2.append(button2, button3);
+    button2.append(icon);
 });
 
+const cards = document.querySelectorAll(".card");
+cards.forEach((btnComprar) => {
+    btnComprar.addEventListener("click", selectorBotones)
+});
 
+function selectorBotones(e) {
+    const texto = e.target.textContent;
+    console.log(texto);
+    if (texto === "Comprar") {
+        comprar(e)
+    } if (texto === "Agregar al Carrito") {
+        agregarCarrito(e)
+    } if (texto === "Ver Carrito") {
+        verCarrito()
+    }
+    return;
+}
 
+function comprar(e) {
+    const boton = e.target;
+    const card = boton.closest(".card");
+    const titulo = card.querySelector(".card-title").textContent;
 
+    products.forEach(producto => {
+        if (producto.nombre === titulo) {
+            carrito.push(producto);
+            const carritoString = JSON.stringify(carrito);
+            localStorage.setItem("carrito", carritoString);
+            console.log(localStorage.getItem("carrito"));
+        }
+    });
 
+    window.location.href = "cart.html"
+
+}
+
+function agregarCarrito(e) {
+    const boton = e.target;
+    const card = boton.closest(".card");
+    const titulo = card.querySelector(".card-title").textContent;
+
+    products.forEach(producto => {
+        if (producto.nombre === titulo) {
+            carrito.push(producto);
+            console.log(carrito);
+        }
+    });
+}
+
+function verCarrito() {
+    const carritoString = JSON.stringify(carrito);
+    localStorage.setItem("carrito", carritoString);
+    // console.log(localStorage.getItem("carrito"));
+    window.location.href = "cart.html"
+}
