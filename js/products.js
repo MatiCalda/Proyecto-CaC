15685cb4f915c3a66d66bd5930075ba62193a9e9
David Ramon Thomen <thomendavid1@gmail.com>
2022-05-09 23:31:29 -0300

    Funciones de botones comprar, agregar y ver carrito en pagina productos realizadas

--- js/products.js
+++ js/products.js
@@ -40,6 +40,7 @@ const products = [
         stock: 50
     }
 ];
+const carrito = [];
 
 products.forEach(product => {
     // Enlazando el div contenedor
@@ -47,6 +48,7 @@ products.forEach(product => {
     // Creacion de las etiquetas con sus clases(.className) y el contenido (.innerText)
     const card = document.createElement("div");
     card.className = "card bg-transparent";
+    card.id = `${product.id}`
 
     const img = document.createElement("img");
     img.className = "card-img-top";
@@ -67,20 +69,98 @@ products.forEach(product => {
     p2.className = "card-text text-light";
     p2.innerText = product.precio;
 
+    const divRight = document.createElement("div");
+    divRight.className = "text-right"
+
     const button = document.createElement("button");
-    button.className = "btn btn-secondary";
-    button.innerText = "Agregar al carrito";
+    button.className = "btn btn-info";
+    button.innerText = "Comprar";
+
+    const divRight2 = document.createElement("div");
+    divRight2.className = "text-right"
+
+    // const div = document.createElement("div");
+    // const div2 = document.createElement("div");
+
+    const button3 = document.createElement("button");
+    button3.className = "badge-pill badge-light py-1";
+    button3.innerText = "Ver Carrito";
+
+    const button2 = document.createElement("button");
+    button2.className = "badge-pill badge-light py-1";
+    button2.innerText = "Agregar al Carrito";
+
+    const icon = document.createElement("i");
+    icon.className = "bi bi-cart-plus ri-xl"
 
     const cardFooter = document.createElement("div");
     cardFooter.className = "card-footer";
 
-    const small = document.createElement("small");
-    small.className = "text-muted";
-    small.innerText = product.stock
-    
+    // const small = document.createElement("small");
+    // small.className = "text-muted";
+    // small.innerText = product.stock
+
     cards.append(card);
     card.append(img, cardBody, cardFooter);
-    cardBody.append(h5, p, p2, button);
-    cardFooter.append(small);
-
+    cardBody.append(h5, p, p2, divRight);
+    divRight.append(button);
+    cardFooter.append(divRight2);
+    divRight2.append(button2, button3);
+    button2.append(icon);
 });
+
+const cards = document.querySelectorAll(".card");
+cards.forEach((btnComprar) => {
+    btnComprar.addEventListener("click", selectorBotones)
+});
+
+function selectorBotones(e) {
+    const texto = e.target.textContent;
+    console.log(texto);
+    if (texto === "Comprar") {
+        comprar(e)
+    } if (texto === "Agregar al Carrito") {
+        agregarCarrito(e)
+    } if (texto === "Ver Carrito") {
+        verCarrito()
+    }
+    return;
+}
+
+function comprar(e) {
+    const boton = e.target;
+    const card = boton.closest(".card");
+    const titulo = card.querySelector(".card-title").textContent;
+
+    products.forEach(producto => {
+        if (producto.nombre === titulo) {
+            carrito.push(producto);
+            const carritoString = JSON.stringify(carrito);
+            localStorage.setItem("carrito", carritoString);
+            console.log(localStorage.getItem("carrito"));
+        }
+    });
+
+    window.location.href = "cart.html"
+
+}
+
+function agregarCarrito(e) {
+    const boton = e.target;
+    const card = boton.closest(".card");
+    const titulo = card.querySelector(".card-title").textContent;
+
+    products.forEach(producto => {
+        if (producto.nombre === titulo) {
+            carrito.push(producto);
+            console.log(carrito);
+        }
+    });
+}
+
+function verCarrito() {
+    const carritoString = JSON.stringify(carrito);
+    localStorage.setItem("carrito", carritoString);
+    console.log(localStorage.getItem("carrito"));
+    window.location.href = "cart.html"
+}
