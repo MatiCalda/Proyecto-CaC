addf9111ced9340af02c10d0adeb864cf35eaff8
David Ramon Thomen <thomendavid1@gmail.com>
2022-05-13 23:12:00 -0300

    Sin cambios

--- js/products.js
+++ js/products.js
@@ -48,142 +48,35 @@ const products = [
         stock: 90
     }
 ];
+const carrito = [];
+let html = "";
 
-class ProductCart {
-    constructor(producto, cantidad) {
-        this.product = producto;
-        this.cantidad = cantidad;
-    }
-};
-
-const cart = [];
-
-// let productosObtenidosDelStorage = JSON.parse(localStorage.getItem("products"));
+// Enlazando el div contenedor
+const cards = document.getElementById("products");
 
 products.forEach(product => {
-    // Enlazando el div contenedor
-    const cards = document.getElementById("products");
-    // Creacion de las etiquetas con sus clases(.className) y el contenido (.innerText)
-    const card = document.createElement("div");
-    card.className = "card bg-light";
-
-    const img = document.createElement("img");
-    img.className = "card-img-top";
-    img.src = product.img;
-
-    const cardBody = document.createElement("div");
-    cardBody.className = "card-body";
-
-    const h5 = document.createElement("h5");
-    h5.className = "card-title";
-    h5.innerText = product.nombre;
-
-    const p = document.createElement("p");
-    p.className = "card-text";
-    p.innerText = product.descripcion;
-
-    const p2 = document.createElement("p");
-    p2.className = "card-text";
-    p2.innerText = product.precio;
-
-    const p3 = document.createElement("p");
-    p3.className = "d-none";
-    p3.innerText = `${product.id}`;
-
-    const divRight = document.createElement("div");
-    divRight.className = "text-right"
-
-    const button = document.createElement("button");
-    button.className = "btnComprar btn btn-info";
-    button.innerText = "Comprar";
-
-    const divRight2 = document.createElement("div");
-    divRight2.className = "text-right"
-
-    // const div = document.createElement("div");
-    // const div2 = document.createElement("div");
-
-    // const button3 = document.createElement("button");
-    // button3.className = "badge-pill badge-light py-1";
-    // button3.innerText = "Ver Carrito";
-
-    const button2 = document.createElement("button");
-    button2.setAttribute("data-id", product.id);
-    button2.addEventListener("click", e => {
-        let idProduct = parseInt(e.target.getAttribute("data-id"));
-        products.forEach(producto => {
-            if (producto.id === idProduct) {
-                let prodCart = new ProductCart(producto, 1);
-                cart.push(prodCart);
-                const carritoString = JSON.stringify(cart);
-                localStorage.setItem("carrito_2", carritoString);
-                console.log(prodCart.product.nombre);
-            }
-        });
-    });
-    button2.className = "btnAgregar  badge-pill badge-light py-1";
-    button2.innerText = "Agregar al Carrito";
-
-    const icon = document.createElement("i");
-    icon.className = "bi bi-cart-plus ri-xl"
-
-    const cardFooter = document.createElement("div");
-    cardFooter.className = "card-footer";
-
-
-    const addCartBtn = document.createElement("button");
-    addCartBtn.setAttribute("data-id", product.id);
-    addCartBtn.addEventListener("click", e => {
-        let idProduct = parseInt(e.target.getAttribute("data-id"));
-
-        products.forEach(producto => {
-            if (producto.id === idProduct) {
-                let prodCart = new ProductCart(producto, 1);
-                cart.push(prodCart);
-                const carritoString = JSON.stringify(cart);
-                localStorage.setItem("carrito_2", carritoString);
-
-            }
-        });
-    });
-
-
-    cards.append(card);
-    card.append(img, cardBody, cardFooter);
-    cardBody.append(h5, p, p2, p3, divRight);
-    divRight.append(button);
-    cardFooter.append(divRight2);
-    divRight2.append(button2);
-    button2.append(icon);
+    // Creacion de las etiquetas con sus clases
+    html +=
+        `    <div class="card bg-light">
+    <img class="card-img-top" src=${product.img}></img>
+    <div class="card-body">
+        <h5 class="card-title">${product.nombre}</h5>
+        <p class="card-text">${product.descripcion}</p>
+        <p class="card-text">${product.precio}</p>
+        <p class="d-none">${product.id}</p>
+        <div class="text-right">
+         <button class="btnComprar btn btn-info" data-id=${product.id}>Comprar</button>
+        </div>
+    </div>
+    <div class="card-footer text-right">
+        <button class="btnAgregar  badge-pill badge-light py-1" data-id=${product.id}>
+        Agregar al Carrito<i class="bi bi-cart-plus ri-xl"></i>
+        </button>
+    </div>
+</div>`;
 });
 
-// Evento comprar
-// Enlazando a lista de botones comprar
-const botonesComprar = document.querySelectorAll(".btnComprar");
-// Recorriendo los botones, agregando la escucha de evento click para lanzar funcion comprar
-botonesComprar.forEach((btn) => {
-    btn.addEventListener("click", comprar)
-});
-
-function comprar(e) {
-    // guardando los datos del boton clickeado
-    const boton = e.target;
-    // Guardando los datos de la card al que pertenece el boton
-    const card = boton.closest(".card");
-    // Guardando el contenido del titulo de la card
-    const titulo = card.querySelector(".card-title").textContent;
-    // Buscando y guardando producto en el carrito y en el localStorage
-    products.forEach(producto => {
-        if (producto.nombre === titulo) {
-            carrito.push(producto);
-            // funcion para guardar el array
-            guardarEnlocalStorage
-        }
-    });
-    // Redirigiendo a carrito
-    window.location.href = "cart.html"
-
-}
+cards.innerHTML = html;
 
 // Evento agregar al carrito
 //  Enlazando todos los botones de agregar
@@ -196,28 +89,19 @@ botonesAgregar.forEach((btn) => {
 function agregarCarrito(e) {
     // guardando los datos del boton clickeado
     const boton = e.target;
-    // Guardando los datos de la card al que pertenece el boton
-    const card = boton.closest(".card");
-    // Guardando el contenido del titulo de la card
-    const titulo = card.querySelector(".card-title").textContent;
+    // Guardando el contenido del atributo data-id del boton
+    const idProduct = parseInt(boton.getAttribute("data-id"));
     // Buscando y guardando producto en el carrito
-    products.forEach(producto => {
-        if (producto.nombre === titulo) {
-            carrito.push(producto);
-            console.log(carrito);
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === idProduct) {
+            carrito.push(products[i]);
+            console.log(carrito)
         }
-    });
+    }
 }
 
-// function verCarrito() {
-//     const carritoString = JSON.stringify(carrito);
-//     localStorage.setItem("carrito", carritoString);
-//     console.log(localStorage.getItem("carrito"));
-//     window.location.href = "cart.html"
-// }
-
 // Evento boton carrito
-const btnCarrito = document.getElementById("carrito");
+const btnCarrito = document.getElementById("btncarrito");
 btnCarrito.addEventListener("click", botonCarrito);
 
 function botonCarrito() {
