97b406e8c24a018ce71a5912c3c1427dd9d5c54f
David Ramon Thomen <thomendavid1@gmail.com>
2022-05-21 17:33:35 -0300

    Merge branch 'main' of https://github.com/MatiCalda/Proyecto-CaC

--- js/cart.js
+++ js/cart.js
@@ -1,17 +1,16 @@
 cart = JSON.parse(localStorage.getItem("cart"));
 
-function mostrarCarrito(){
+function mostrarCarrito() {
     var total = 0;
     document.getElementById('carrito').innerHTML = '';
     cart.forEach(pedido => {
         var subTotal;
         subTotal = pedido.product.precio * pedido.cantidad;
         total += subTotal;
-        console.log(subTotal);
         let html = `
             <tr id="${pedido.product.id}">
                 <td>
-                    <img src="https://picsum.photos/100" class="avatar img-circle img-thumbnail" alt="avatar">
+                    <img src="${pedido.product.img}" class="avatar img-circle img-thumbnail img-cart" alt="avatar">
                 </td>
                 <td class="align-middle">
                     ${pedido.product.nombre}
@@ -22,11 +21,11 @@ function mostrarCarrito(){
                 <td class="align-middle text-center">
                     ${pedido.cantidad}
                 </td>
-                <td class="align-middle">
-                    $ ${pedido.product.precio}
+                <td class="align-middle text-right">
+                    $&nbsp;${pedido.product.precio}
                 </td>
-                <td class="align-middle">
-                    $ ${subTotal}
+                <td class="align-middle text-right">
+                    $&nbsp;${subTotal}
                 </td>
                 <td class="align-middle">
                     <a class="btnEliminar ri-delete-bin-2-line text-decoration-none ri-lg" data-id="${pedido.product.id}"></a>
@@ -35,58 +34,72 @@ function mostrarCarrito(){
                 `;
         document.getElementById('carrito').innerHTML += html;
     });
-    
+
     actualizarTotal(total);
 
     const botonesElminar = document.querySelectorAll(".btnEliminar");
     // Recorriendo los botones, agregando la escucha de evento click para lanzar funcion Elminar
     botonesElminar.forEach((btn) => {
-        btn.addEventListener("click", e=>{
+        btn.addEventListener("click", e => {
             let idProduct = parseInt(e.target.getAttribute("data-id"));
             let precioProducto;
-    
+
             cart.forEach(pedido => {
-                if(pedido.product.id === idProduct){
-                    console.log('producto encontrado: $' + pedido.product.precio);
+                if (pedido.product.id === idProduct) {
                     precioProducto = pedido.product.precio;
-    
-                    if(pedido.cantidad === 1){
+
+                    if (pedido.cantidad === 1) {
                         cart = removeItemOnce(cart, pedido);
                         const borrar = document.getElementById(idProduct);
                         borrar.parentNode.removeChild(borrar);
-                    }else{
+                    } else {
                         pedido.cantidad--;
                     }
-    
+
                     const carritoString = JSON.stringify(cart);
                     localStorage.setItem("cart", carritoString);
                 }
             });
-    
-    
+
+
             total -= precioProducto;
             mostrarCarrito();
-            console.log(idProduct);
         })
-    });    
+    });
+    const btnComprar = document.getElementById("btnComprar");
+    if (cart.length == 0) {
+        btnComprar.setAttribute("disabled", true);
+    }else{
+        btnComprar.removeAttribute("disabled");
+    }
+
 }
 mostrarCarrito();
+
+const borrarTodoCarrito = document.getElementById("btnBorrarTodo");
+borrarTodoCarrito.addEventListener("click", e => {
+    cart = [];
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    mostrarCarrito();
+})
+
+
 
 
 
 function removeItemOnce(arr, value) {
     var index = arr.indexOf(value);
     if (index > -1) {
-      arr.splice(index, 1);
+        arr.splice(index, 1);
     }
     return arr;
-  }
+}
 
 function actualizarTotal(totalCalculado) {
-    console.log(totalCalculado);
     let html = `
         <th scope="col">
-            $ ${totalCalculado}
+            $${totalCalculado}
         </th>
     `;
     document.getElementById('total').innerHTML = html;
--- js/signIn.js
+++ js/signIn.js
@@ -1,138 +1,213 @@
-var tieneErrores = true;
+// validaciones SignIn
+
+var esValido = {
+    nombre: false,
+    apellido: false,
+    alias: false,
+    codigoPostal: false,
+    email: false,
+    verifEmail: false,
+    clave: false,
+    verifClave: false,
+}
+
+habilitarSignIn();
 
 document.getElementById("nombre").addEventListener('focusout', e => {
     let campo = e.target.value;
     if (campo.length < 3) {
-        tieneErrores = true;
         e.target.classList.add("alert-danger");
         e.target.classList.add("border-danger");
         document.getElementById("nombreWarning").innerText = "Nombre muy corto.";
+        esValido["nombre"] = false;
     }
-    else{
-        tieneErrores = false;
+    else {
         e.target.classList.remove("border-danger");
         e.target.classList.remove("alert-danger");
         document.getElementById("nombreWarning").innerHTML = "&nbsp;";
+        esValido["nombre"] = true;
     }
+    habilitarSignIn();
 });
 
 document.getElementById("apellido").addEventListener('focusout', e => {
     let campo = e.target.value;
     if (campo.length < 3) {
-        tieneErrores = true;
         e.target.classList.add("alert-danger");
         e.target.classList.add("border-danger");
         document.getElementById("apellidoWarning").innerText = "Apellido muy corto.";
+        esValido["apellido"] = false;
     }
-    else{
-        tieneErrores = false;
+    else {
         e.target.classList.remove("border-danger");
         e.target.classList.remove("alert-danger");
         document.getElementById("apellidoWarning").innerHTML = "&nbsp;";
+        esValido["apellido"] = true;
     }
+    habilitarSignIn();
 });
 
 document.getElementById("alias").addEventListener('focusout', e => {
     let campo = e.target.value;
     if (campo.length == 0) {
-        tieneErrores = true;
         e.target.classList.add("alert-danger");
         e.target.classList.add("border-danger");
         document.getElementById("aliasWarning").innerText = "Alias muy corto.";
+        esValido["alias"] = false;
     }
-    else{
-        tieneErrores = false;
+    else {
         e.target.classList.remove("border-danger");
         e.target.classList.remove("alert-danger");
         document.getElementById("aliasWarning").innerHTML = "&nbsp;";
+        esValido["alias"] = true;
     }
+    habilitarSignIn();
 });
 
 document.getElementById("codigoPostal").addEventListener('focusout', e => {
     let campo = e.target.value;
     if (campo.length < 4) {
-        tieneErrores = true;
         e.target.classList.add("alert-danger");
         e.target.classList.add("border-danger");
         document.getElementById("codigoPostalWarning").innerText = "El codigo postal debe tener mínimo 4 digitos.";
+        esValido["codigoPostal"] = false;
     }
-    else{
-        tieneErrores = false;
+    else {
         e.target.classList.remove("border-danger");
         e.target.classList.remove("alert-danger");
         document.getElementById("codigoPostalWarning").innerHTML = "&nbsp;";
+        esValido["codigoPostal"] = true;
     }
+    habilitarSignIn();
 });
 
 document.getElementById("email").addEventListener('focusout', e => {
     let campo = e.target.value;
     if (!campo.includes("@") || !campo.includes(".com")) {
-        tieneErrores = true;
         e.target.classList.add("alert-danger");
         e.target.classList.add("border-danger");
         document.getElementById("emailWarning").innerText = "Email no válido";
+        esValido["email"] = false;
     }
-    else{
-        tieneErrores = false;
+    else {
         e.target.classList.remove("border-danger");
         e.target.classList.remove("alert-danger");
         document.getElementById("emailWarning").innerHTML = "&nbsp;";
+        esValido["email"] = true;
     }
+    habilitarSignIn();
 });
 
 document.getElementById("verifEmail").addEventListener('focusout', e => {
     let campo = e.target.value;
     if (campo != document.getElementById("email").value) {
-        tieneErrores = true;
         e.target.classList.add("alert-danger");
         e.target.classList.add("border-danger");
         document.getElementById("verifEmailWarning").innerText = "El email no corresponde con el anterior";
+        esValido["verifEmail"] = false;
     }
-    else{
-        tieneErrores = false;
+    else {
         e.target.classList.remove("border-danger");
         e.target.classList.remove("alert-danger");
         document.getElementById("verifEmailWarning").innerHTML = "&nbsp;";
+        esValido["verifEmail"] = true;
     }
+    habilitarSignIn();
 });
 
 document.getElementById("clave").addEventListener('focusout', e => {
     let campo = e.target.value;
     if (campo.length < 6) {
-        tieneErrores = true;
         document.getElementById("claveWarning").className = "warning";
         e.target.classList.add("alert-danger");
         e.target.classList.add("border-danger");
+        esValido["clave"] = false;
     }
-    else{
-        tieneErrores = false;
+    else {
         document.getElementById("claveWarning").className = "light";
         e.target.classList.remove("border-danger");
         e.target.classList.remove("alert-danger");
+        esValido["clave"] = true;
     }
+    habilitarSignIn();
 });
 
 document.getElementById("verifClave").addEventListener('focusout', e => {
     let campo = e.target.value;
     if (campo != document.getElementById("clave").value) {
-        tieneErrores = true;
         e.target.classList.add("alert-danger");
         e.target.classList.add("border-danger");
         document.getElementById("verifClaveWarning").innerText = "La contraseña no corresponde con la anterior";
+        esValido["verifClave"] = false;
     }
-    else{
-        tieneErrores = false;
+    else {
         e.target.classList.remove("border-danger");
         e.target.classList.remove("alert-danger");
         document.getElementById("verifClaveWarning").innerHTML = "&nbsp;";
+        esValido["verifClave"] = true;
     }
+    habilitarSignIn();
 });
 
-document.getElementById("form").addEventListener('submit', e =>{
-    if (tieneErrores) {
-        e.preventDefault();
-        alert("no envio");
-    }else{
-        e.target.submit();
+function camposValidos() {
+    let valido = true;
+    let valores = Object.values(esValido);
+    for (let i = 0; i < valores.length; i++) {
+        valido &&= valores[i];
+    }
+    return valido;
+}
+
+function habilitarSignIn() {
+    const btnSignIn = document.getElementById("btnSignIn");
+    if (camposValidos()) {
+        btnSignIn.removeAttribute("disabled");
+    } else {
+        btnSignIn.setAttribute("disabled", true);
+    }
+}
+
+// SignIn
+
+let users = [];
+users = obtenerUsuarios();
+
+const btnSingIn = document.getElementById("btnSignIn");
+btnSingIn.addEventListener("click", e => {
+    e.preventDefault();
+    if (buscarUsuario(document.getElementById("email").value) != undefined) { // si el usuario existe
+        const correo = document.getElementById("emailWarning");
+        correo.innerText = "El mail ya está registrado";
+    } else {
+        let user = {};
+        user.nombre = document.getElementById("nombre").value;
+        user.apellido = document.getElementById("apellido").value;
+        user.alias = document.getElementById("alias").value;
+        user.codigoPostal = document.getElementById("codigoPostal").value;
+        user.email = document.getElementById("email").value;
+        user.clave = document.getElementById("clave").value;
+        users.push(user);
+        localStorage.setItem("users", JSON.stringify(users));
+        // mostrar modal
+        $('#msjOK').modal({
+            backdrop: 'static'
+        })
     }
 })
+
+function buscarUsuario(email) {
+    let user = users.find(user => { return user.email === email; });
+    return user;
+}
+
+function obtenerUsuarios() {
+    let users = [];
+    if (localStorage.getItem('users') == undefined) { // si no existe el carrito
+        localStorage.setItem("users", JSON.stringify(users));
+    } else {
+        users = JSON.parse(localStorage.getItem("users"));
+    }
+    return users;
+}
+
+
