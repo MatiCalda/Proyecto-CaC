0fe4ebd72d8bc72a8a4ad1f22d2f42ef12915c98
MatiasCaldarone <matias.caldarone@gmail.com>
2022-05-21 19:55:21 -0300

    login (solo en products.js)

--- js/logIn.js
+++ js/logIn.js
@@ -5,20 +5,40 @@ users = obtenerUsuarios();
 const btnLogIn = document.getElementById("btnLogIn");
 btnLogIn.addEventListener("click", e => {
     e.preventDefault();
-    let mail = document.getElementById("email");
-    let pass = document.getElementById("password");
+    let mail = document.getElementById("email").value;
+    let pass = document.getElementById("password").value;
     let ingresoValido = false;
-    users.forEach( e => {
+    users.forEach(e => {
         if (e.email == mail && e.clave == pass) {
             ingresoValido = true;
         }
     })
     if (ingresoValido) {
-        alert("ingreso exitoso");
-    }else{
-        
+        let html = document.getElementById("msjEstado");
+        html.innerHTML = `
+        <div class="alert alert-sucsess" role="alert">
+            Acceso exitoso
+        </div>
+        `
+        $('#login').modal('hide');
+        //alert("ingreso exitoso");
+    } else {
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+        let html = document.getElementById("msjEstado");
+        html.innerHTML = `
+        <div class="alert alert-danger" role="alert">
+            Dato/s erróneos, verificar mail y/o contraseña.
+        </div>
+        `
+        //alert("error en ingreso");
     }
 
+})
+
+document.getElementById("closeLogIn").addEventListener("click", e => {
+    let html = document.getElementById("msjEstado");
+        html.innerHTML = ` `
 })
 
 function obtenerUsuarios() {
