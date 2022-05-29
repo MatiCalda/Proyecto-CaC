7f5fafdf7780d12c52a66afb8f6ef3f8b3e4b6de
David Ramon Thomen <thomendavid1@gmail.com>
2022-05-29 20:07:52 -0300

    Primer version de filtro realizado

--- js/products.js
+++ js/products.js
@@ -7,6 +7,48 @@ class ProductCart {
 
 const cart = obtenerCarrito();
 const products = obtenerProductos();
+console.log(products);
+obtenerCards(products);
+
+// Enlazamos las categorias
+const celulares = document.getElementById("celulares");
+const accesorios = document.getElementById("accesorios");
+const pc = document.getElementById("pc");
+const notebook = document.getElementById("notebook");
+const componentes = document.getElementById("componentes");
+
+// Agregamos eventos a los links
+celulares.addEventListener("click", e => {
+    e.preventDefault();
+    let valor = e.target.getAttribute("id");
+    productos = products.filter(p => p.categoria == valor);
+    obtenerCards(productos);
+});
+accesorios.addEventListener("click", e => {
+    e.preventDefault();
+    let valor = e.target.getAttribute("id");
+    productos = products.filter(p => p.categoria == valor);
+    obtenerCards(productos);
+});
+pc.addEventListener("click", e => {
+    e.preventDefault();
+    let valor = e.target.getAttribute("id");
+    productos = products.filter(p => p.categoria == valor);
+    obtenerCards(productos);
+});
+notebook.addEventListener("click", e => {
+    e.preventDefault();
+    let valor = e.target.getAttribute("id");
+    productos = products.filter(p => p.categoria == valor);
+    obtenerCards(productos);
+});
+componentes.addEventListener("click", e => {
+    e.preventDefault();
+    let valor = e.target.getAttribute("id");
+    productos = products.filter(p => p.categoria === valor);
+    obtenerCards(productos);
+});
+
 
 function obtenerCarrito() {
     let theCart = [];
@@ -18,89 +60,93 @@ function obtenerCarrito() {
     return theCart;
 }
 
-function obtenerProductos(){
+function obtenerProductos() {
     let products;
     products = JSON.parse(localStorage.getItem("products"));
     return products;
 }
-
-products.forEach(product => {
+function obtenerCards(productos) {
     // Enlazando el div contenedor
     const cards = document.getElementById("products");
-    // Creacion de las etiquetas con sus clases(.className) y el contenido (.innerText)
-    const card = document.createElement("div");
-    card.className = "card bg-light";
+    // Limpiando el contenedor
+    cards.innerHTML="";
+    productos.forEach(product => {
+        // Creacion de las etiquetas con sus clases(.className) y el contenido (.innerText)
+        const card = document.createElement("div");
+        card.className = "card bg-light";
 
-    const img = document.createElement("img");
-    img.className = "card-img-top";
-    img.src = "../img/Productos/"+product.img;
-    
-    const cardBody = document.createElement("div");
-    cardBody.className = "card-body d-flex flex-column";
-   
-    const h5 = document.createElement("h5");
-    h5.className = "card-title";
-    h5.innerText = product.nombre;
+        const img = document.createElement("img");
+        img.className = "card-img-top";
+        img.src = "../img/Productos/" + product.img;
 
-    const p = document.createElement("p");
-    p.className = "card-text";
-    p.innerText = product.descripcion;
+        const cardBody = document.createElement("div");
+        cardBody.className = "card-body d-flex flex-column";
 
-    const p2 = document.createElement("p");
-    p2.className = "card-text";
-    p2.innerText = "$ " + product.precio.toLocaleString("en");
+        const h5 = document.createElement("h5");
+        h5.className = "card-title";
+        h5.innerText = product.nombre;
 
-    const p3 = document.createElement("p");
-    p3.className = "d-none";
-    p3.innerText = `${product.id}`;
+        const p = document.createElement("p");
+        p.className = "card-text";
+        p.innerText = product.descripcion;
 
-    const divRight = document.createElement("div");
-    divRight.className = "mt-auto text-right"
+        const p2 = document.createElement("p");
+        p2.className = "card-text";
+        p2.innerText = "$ " + product.precio.toLocaleString("en");
 
-    const button = document.createElement("button");
-    button.setAttribute("data-toggle", "modal");
-    button.setAttribute("data-target", "#formaDePago");
-    button.className = "btnComprar btn btn-info";
-    button.innerText = "Comprar";
+        const p3 = document.createElement("p");
+        p3.className = "d-none";
+        p3.innerText = `${product.id}`;
 
-    const divRight2 = document.createElement("div");
-    divRight2.className = "text-right"
+        const divRight = document.createElement("div");
+        divRight.className = "mt-auto text-right"
 
-    const button2 = document.createElement("a");
-    button2.href = ("javascript:void(0)");
-    button2.setAttribute("data-id", product.id);
-    button2.className = "btnAgregar text-decoration-none badge-pill badge-light py-1";
-    button2.innerText = "Agregar al Carrito";
-    button2.addEventListener("click", e => { // agrega el producto al carrito
-        let idProduct = parseInt(e.target.getAttribute("data-id"));
-        products.forEach(producto => {
-            if (producto.id === idProduct) {
-                let prodCart = new ProductCart(producto, 1);
-                let itemCarrito = cart.find(item => { return item.product.id === idProduct; });
-                if (itemCarrito == undefined) {   // si no encontro el elemento en el carrito
-                    cart.push(prodCart);
-                } else {
-                    itemCarrito.cantidad++;
+        const button = document.createElement("button");
+        button.setAttribute("data-toggle", "modal");
+        button.setAttribute("data-target", "#formaDePago");
+        button.className = "btnComprar btn btn-info";
+        button.innerText = "Comprar";
+
+        const divRight2 = document.createElement("div");
+        divRight2.className = "text-right"
+
+        const button2 = document.createElement("a");
+        button2.href = ("javascript:void(0)");
+        button2.setAttribute("data-id", product.id);
+        button2.className = "btnAgregar text-decoration-none badge-pill badge-light py-1";
+        button2.innerText = "Agregar al Carrito";
+        button2.addEventListener("click", e => { // agrega el producto al carrito
+            let idProduct = parseInt(e.target.getAttribute("data-id"));
+            products.forEach(producto => {
+                if (producto.id === idProduct) {
+                    let prodCart = new ProductCart(producto, 1);
+                    let itemCarrito = cart.find(item => { return item.product.id === idProduct; });
+                    if (itemCarrito == undefined) {   // si no encontro el elemento en el carrito
+                        cart.push(prodCart);
+                    } else {
+                        itemCarrito.cantidad++;
+                    }
+                    const carritoString = JSON.stringify(cart);
+                    console.log(carritoString);
+                    localStorage.setItem("cart", carritoString);
                 }
-                const carritoString = JSON.stringify(cart);
-                console.log(carritoString);
-                localStorage.setItem("cart", carritoString);
-            }
-            actualizarIconoCarrito();
+                actualizarIconoCarrito();
+            });
         });
+
+        const icon = document.createElement("i");
+        icon.className = "bi bi-cart-plus ri-xl"
+
+        const cardFooter = document.createElement("div");
+        cardFooter.className = "card-footer";
+
+        cards.append(card);
+        card.append(img, cardBody, cardFooter);
+        cardBody.append(h5, p, divRight);
+        divRight.append(p2, button);
+        cardFooter.append(divRight2);
+        divRight2.append(button2);
+        button2.append(icon);
     });
-    
-    const icon = document.createElement("i");
-    icon.className = "bi bi-cart-plus ri-xl"
+}
 
-    const cardFooter = document.createElement("div");
-    cardFooter.className = "card-footer";
-
-    cards.append(card);
-    card.append(img, cardBody, cardFooter);
-    cardBody.append(h5, p, divRight);
-    divRight.append(p2,button);
-    cardFooter.append(divRight2);
-    divRight2.append(button2);
-    button2.append(icon);
-});
